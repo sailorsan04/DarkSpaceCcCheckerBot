f1858576fc6a9f08a08b0504d63987f770e0ff06
sailorsan02 <leebeverly293@gmail.com>
2025-03-13 11:11:33 -0400

    Initial commit: Added all bot files

---
+++ config/config.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Bot Token
+|--------------------------------------------------------------------------
+|
+| Change this to your Bot API Token
+| It can be obtained from https://telegram.dog/BotFather
+|
+*/
+$config['botToken'] = getenv('BOT_TOKEN');
+
+/*
+|--------------------------------------------------------------------------
+| Admin User ID
+|--------------------------------------------------------------------------
+|
+| Change this to Admin's Numeric User ID
+| ID can be obtained from https://telegram.dog/username_to_id_bot
+|
+*/
+$config['adminID'] = getenv('ADMIN_ID');
+
+/*
+|--------------------------------------------------------------------------
+| Logs Channel ID
+|--------------------------------------------------------------------------
+|
+| Create a New Channel/Group for logging data
+| ID can be obtained from https://telegram.dog/BotFather
+|
+*/
+$config['logsID'] =  getenv('LOGS_ID');
+
+/*
+|--------------------------------------------------------------------------
+| Timezone
+|--------------------------------------------------------------------------
+|
+| Current timezone for Logging Activities with time
+| It can be obtained from http://1min.in/content/international/time-zones
+| By Default it's in IST
+|
+*/
+$config['timeZone'] =  getenv('TIMEZONE') ?: "Asia/Kolkata";
+
+/*
+|--------------------------------------------------------------------------
+| Database
+|--------------------------------------------------------------------------
+| Database to Store User Data
+|
+*/
+$config['db']['hostname'] =  getenv('DB_HOST');
+$config['db']['username'] =  getenv('DB_USER');
+$config['db']['password'] =  getenv('DB_PASS');
+$config['db']['database'] =  getenv('DB_NAME');
+
+/*
+|--------------------------------------------------------------------------
+| Anti-Spam Timer
+|--------------------------------------------------------------------------
+|
+| Anti-Spam Timer to prevent Spammers from Spamming the Checker
+| Value is in Seconds. "20" = 20seconds
+|
+*/
+$config['anti_spam_timer'] =  getenv('ANTI_SPAM_TIMER') ?: "20";
+
+/*
+|--------------------------------------------------------------------------
+| SK Keys
+|--------------------------------------------------------------------------
+|
+| SK Keys for !sm checker gate
+| Add a Live SK Key here. You can Also add Multiple SK Keys
+| array('sk1','sk2','sk3')
+|
+*/
+$config['sk_keys'] =  explode(',', getenv('SK_KEYS'));
+
+?>
